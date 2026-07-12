b1c397d2a3faa107f7cd681afbee7e9c77f9fd8d
Richard Angelo <richardangelo55@gmail.com>
2026-07-12 14:17:00 -0300

    Adicionando auth-backend.js e corrigindo server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 const db = require('./database');
-const auth = require('./auth');
+const auth = require('./auth-backend');
 require('dotenv').config();
 
 const app = express();
